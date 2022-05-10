ae92b4073bb1576aff56a4a3cb63ee6f9f712bf1
Patrick Young <patrickyoung281@gmail.com>
2022-05-10 17:47:37 -0400

    commit tree b3a76a193f1b17101023b1aab5910f5c0673898e

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const main = document.getElementById("main");
+
+main.remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "YOUR-NAME is the champion";
+
+
+
+
